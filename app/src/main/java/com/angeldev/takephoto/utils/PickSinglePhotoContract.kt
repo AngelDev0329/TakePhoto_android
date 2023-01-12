48e18917fbcf0dcdffad00fbdaaa1662bf40f7d6
AngelDev727 <54489723+AngelDev727@users.noreply.github.com>
2023-01-13 06:10:38 +0800

    tegrating MVVM - 3

--- app/src/main/java/com/angeldev/takephoto/utils/PickSinglePhotoContract.kt
+++ app/src/main/java/com/angeldev/takephoto/utils/PickSinglePhotoContract.kt
@@ -5,7 +5,12 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.MediaStore
+import androidx.activity.ComponentActivity
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import com.angeldev.takephoto.mvvms.AppViewModel
 
 
 private const val MIME_TYPE_IMAGE = "image/*"
@@ -23,5 +28,19 @@ class PickSinglePhotoContract: ActivityResultContract<Void?, Uri?>() {
 
     override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
         return intent.takeIf { resultCode == Activity.RESULT_OK }?.data
+    }
+}
+
+
+class PhotoPickerActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<AppViewModel>()
+
+    private val singlePhotoPickerLauncher = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { imageUri: Uri? ->
+        imageUri?.let(viewModel::setImageUri)
+    }
+
+    private fun pickPhoto() {
+        singlePhotoPickerLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
 }
